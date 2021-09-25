af0651eee1bbefef5e3d11c98308ded4a1afa805
Ilya Benikov <ilyabenikov@MacBook-Pro-Ilya.local>
2021-09-25 21:10:29 +0300

    Created project. Created class CoffeeBox. Created menu.

---
+++ cmake-build-debug/CMakeFiles/CoffeeBox.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CoffeeBox.dir/main.cpp.o"
+  "CoffeeBox"
+  "CoffeeBox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CoffeeBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+int getNum();
+
+class CoffeBox{
+private:
+    int balance;
+    int cup;
+
+public:
+    CoffeBox(int balance, int cup) : balance(balance), cup(cup) {
+        this->balance = balance;
+        this->cup = cup;
+    }
+
+public:
+    int getBalance(){
+        return balance;
+    }
+    int depositCoin(int coin){
+        balance += coin;
+        return balance;
+    }
+};
+
+int main() {
+
+    CoffeBox coffeBox(0, 7);
+
+    while (true){
+
+        cout << "Coffee Box. v1.0" << endl;
+        cout << "Balance: " << coffeBox.getBalance() << "byn." << endl;
+        cout << "1. Add money." << endl;
+        cout << "2. Make Americano." << endl;
+        cout << "3. Make Cappuccino." << endl;
+        cout << "4. Make Espresso." << endl;
+        cout << "5. Service menu." << endl;
+        cout << "(c) 2021, by Hanexical, w_myslicki" << endl << endl;
+
+        switch (getNum()) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+        }
+    }
+
+    return 0;
+}
